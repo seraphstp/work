673838de43be09cbafebacd4d62f6613f6dfefee
seraphst <stupin.maxim2014@andex.ru>
2023-12-14 23:20:53 +0300

    Initial Commit

---
+++ am.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int f (int n) {
+if(n==0) return 0;
+if(n==1 || n==2) return 1;
+cout<<"\n Ступин М.Е. иэ-63-22";
+string st="asdaasd";
+int a=3;
+return f(n-1)+f(n-2);
+}
+
+int main()
+{
+cout<<f(5)<<endl;
+int var1=0;
+int var2=1;
+string str="sdassd";
+return 0;
+}
